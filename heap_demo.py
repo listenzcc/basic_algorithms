22ea0c505a68af3d08410b566a4471ac98dddede
zcc <listenzcc@mail.bnu.edu.cn>
2019-02-15 18:25:30 +0800

    modify heap_demo, better print graph

--- heap_demo.py
+++ heap_demo.py
@@ -10,18 +10,43 @@ class Heap:
     def len(self):
         return len(self.nums)
 
-    def add(self, x):
+    def append(self, x):
         self.nums.append(x)
 
+    def legal_j(self, j):
+        if j < 0:
+            return False
+        if j >= self.len():
+            return False
+        return True
+
+    def print_node(self, j):
+        assert(self.legal_j(j))
+        print(','.join(str(self.nums[x])
+                       for x in [j, j*2+1, j*2+2] if self.legal_j(x)))
+
+    def print_heap(self):
+        print('-' * 60)
+        print(self.len())
+        print(self.nums)
+        for j in range(self.len()):
+            self.print_node(j)
+
     def print_as_graph(self):
+        print('-' * 60)
         print(self.len())
         print(self.nums)
 
         def pnt(x, n, end=''):
             sx = str(x)
             left = int((n - len(sx)) / 2)
+            left_1 = int(left/2)
+            left_2 = left - left_1
             right = n - left - len(sx)
-            print('.'*left + sx + '.'*right, end=end)
+            right_1 = int(right/2)
+            right_2 = right - right_1
+            print(' '*left_1 + '_'*left_2 + sx +
+                  '_'*right_1 + ' '*right_2, end=end)
 
         line = 0
         tmp = self.len()
@@ -43,5 +68,6 @@ class Heap:
 
 
 heap = Heap()
-any(heap.add(np.random.randint(1000)) for _ in range(30))
+any(heap.append(np.random.randint(1000)) for _ in range(20))
 heap.print_as_graph()
+heap.print_heap()
