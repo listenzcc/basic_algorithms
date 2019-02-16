b7cc106a4faa37b6e8f7c7104bb4f5577518fec3
zcc <listenzcc@mail.bnu.edu.cn>
2019-02-16 16:20:51 +0800

    finish heap demo.

--- heap_demo.py
+++ heap_demo.py
@@ -6,12 +6,19 @@ import numpy as np
 class Heap:
     def __init__(self):
         self.nums = []
+        self.length = 0
 
     def len(self):
-        return len(self.nums)
+        return self.length
 
     def append(self, x):
         self.nums.append(x)
+        self.length += 1
+
+    def stack(self, j):
+        self.swap(j, self.len()-1)
+        self.length -= 1
+        self.adjust_heap()
 
     def legal_j(self, j):
         if j < 0:
@@ -20,10 +27,45 @@ class Heap:
             return False
         return True
 
+    def swap(self, j, k):
+        assert(self.legal_j(j))
+        assert(self.legal_j(k))
+        temp = self.nums[j]
+        self.nums[j] = self.nums[k]
+        self.nums[k] = temp
+
+    def adjust(self, j):
+        if not self.legal_j(j):
+            return False
+        a = j*2+1
+        b = j*2+2
+        if not self.legal_j(a):
+            return False
+        if not self.legal_j(b):
+            if self.nums[a] > self.nums[j]:
+                self.swap(j, a)
+                return a
+        else:
+            if self.nums[a] < self.nums[b]:
+                if self.nums[b] > self.nums[j]:
+                    self.swap(j, b)
+                    return b
+            else:
+                if self.nums[a] > self.nums[j]:
+                    self.swap(j, a)
+                    return a
+        return False
+
+    def adjust_heap(self):
+        for j in range(self.len()-1, -1, -1):
+            jj = self.adjust(j)
+            while jj:
+                jj = self.adjust(jj)
+
     def print_node(self, j):
         assert(self.legal_j(j))
-        print(','.join(str(self.nums[x])
-                       for x in [j, j*2+1, j*2+2] if self.legal_j(x)))
+        print('%d: ' % j + ','.join(str(self.nums[x])
+                                    for x in [j, j*2+1, j*2+2] if self.legal_j(x)))
 
     def print_heap(self):
         print('-' * 60)
@@ -34,7 +76,7 @@ class Heap:
 
     def print_as_graph(self):
         print('-' * 60)
-        print(self.len())
+        print(len(self.nums))
         print(self.nums)
 
         def pnt(x, n, end=''):
@@ -49,13 +91,12 @@ class Heap:
                   '_'*right_1 + ' '*right_2, end=end)
 
         line = 0
-        tmp = self.len()
+        tmp = len(self.nums)
         while tmp > 0:
             tmp -= 2 ** line
             line += 1
         line -= 1
         total = 2 ** line * 6
-
         nums = self.nums.copy()
         nums.reverse()
         for ln in range(line+1):
@@ -69,5 +110,10 @@ class Heap:
 
 heap = Heap()
 any(heap.append(np.random.randint(1000)) for _ in range(20))
-heap.print_as_graph()
 heap.print_heap()
+heap.print_as_graph()
+heap.adjust_heap()
+heap.print_as_graph()
+while heap.len():
+    heap.stack(0)
+heap.print_as_graph()
